fdd4b30991b5ae91b5d53fad294468ffef675b69
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-26 22:42:41 +0000

    Adiciona exemplos sobre

---
+++ wrapper/Wrapper.java
@@ -0,0 +1,18 @@
+package wrapper;
+
+public class Wrapper {
+    public static void main(String[] args) {
+
+        // Primitivos
+        int tipoInt = 1;
+        float tipoFloat = 1;
+        boolean tipoBoolean = false;
+
+        // int - char
+        Float tipoFloat2 = 1F;
+        Boolean tiBoolean2  = false;
+        Integer tipoInteger = 1;
+        
+
+    }
+}
